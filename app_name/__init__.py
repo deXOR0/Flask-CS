6675cc575f65a465b7b376452ef1d180a13d096f
Rig <rig@pop-os.localdomain>
2020-12-27 08:45:48 +0700

    Added Flask-Migrate

--- app_name/__init__.py
+++ app_name/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask #Basic flask dependecies
 from flask_sqlalchemy import SQLAlchemy #Database connection and ORM
 from flask_bcrypt import Bcrypt #Hashing and CSRF token
 from flask_login import LoginManager #Login Manager
+from flask_migrate import Migrate 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = ''
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 # Uncomment the two lines below to implement default login page
